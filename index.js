08907b0d7634ef0adc482b5ec76cc18d1de57610
Yogendra Rajpoot <90867615+YogendraRajpoot@users.noreply.github.com>
2023-04-17 04:45:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const connectToMongo = require("./db");
 const cors = require("cors");
+require("dotenv").config();
 
 connectToMongo();
 
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 
